c762d9d42c7bba487cf670c39b69cbc7483607f0
Sebastián Salata R-T <SA.SalataRT@GMail.com>
2016-02-03 15:13:19 -0300

    main layout

--- server/publications.js
+++ server/publications.js
@@ -2,8 +2,8 @@ Meteor.publish('users', () => {
   var options = {
     fields: {
       _id: 1,
-      'services.facebook.profile.name': 1,
-      'services.facebook.profile.picture': 1,
+      'profile.name': 1,
+      'profile.picture': 1,
     }
   };
   Meteor.users.find({}, options);
