50940a2a483086a5115b7a58615b1884ab568bc7
Sebastián Salata R-T <SA.SalataRT@GMail.com>
2016-02-04 17:21:52 -0300

    add 'course' page and adapt seed

--- client/lib/router.js
+++ client/lib/router.js
@@ -2,6 +2,15 @@ Router.configure({
   layoutTemplate: 'appLayout'
 });
 
-Router.route('/', function () {
-  this.render('landingPage');
+Router.route('/', {
+  template: 'landingPage'
 });
+
+Router.route('/courses/:_id', {
+  name: 'coursePage',
+  template: 'course',
+  data: function() {
+    currentCourse = this.params._id;
+    return Courses.findOne({ _id: currentCourse });
+  }
+})
