d65e02b5c1d3e6702b6bc05167f212d7723a9e4f
Sebastián Salata R-T <SA.SalataRT@GMail.com>
2016-02-05 17:36:21 -0300

    implement testimonies

--- client/templates/login/login.js
+++ client/templates/login/login.js
@@ -1,11 +1,11 @@
 Template.login.events({
-  'click #facebook-login': event => {
+  'click #facebook-login': function(event) {
     Meteor.loginWithFacebook({}, err => {
       if (err) { throw new Meteor.Error("Facebook login failed"); }
     });
   },
 
-  'click #logout': event => {
+  'click #logout': function(event) {
     Meteor.logout(err => {
       if (err) { throw new Meteor.Error("Logout failed"); }
     });
--- server/social-config.js
+++ server/social-config.js
@@ -1,17 +1,17 @@
 ServiceConfiguration.configurations.remove({
-    service: 'facebook'
+  service: 'facebook'
 });
 
 ServiceConfiguration.configurations.insert({
-    service: 'facebook',
-    appId: process.env.SPEKI_FB_ID,
-    secret: process.env.SPEKI_FB_SECRET
+  service: 'facebook',
+  appId: process.env.SPEKI_FB_ID,
+  secret: process.env.SPEKI_FB_SECRET
 });
 
 Accounts.onCreateUser((options, user) => {
-    if (options.profile) {
-        options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=normal";
-        user.profile = options.profile;
-    }
-    return user;
+  if (options.profile) {
+    options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=normal";
+    user.profile = options.profile;
+  }
+  return user;
 });
