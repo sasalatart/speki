7d5832f1265d1d697ed073371da43dfdb1457a82
Sebastián Salata R-T <SA.SalataRT@GMail.com>
2016-02-08 12:39:23 -0300

    implement course creation, edition and deletion

---
+++ client/templates/courses/editCourse/editCourse.js
@@ -0,0 +1,21 @@
+Template.editCourse.events({
+  'submit .edit-course-form': function(event) {
+    event.preventDefault();
+
+    var initials = event.target.initials.value;
+    var name = event.target.name.value;
+    var school = event.target.school.value;
+    var information = event.target.information.value;
+
+    Meteor.call('updateCourse', this._id, initials, name, school, information);
+    $('.ui.modal').modal('hide');
+  }
+});
+
+Template.editCourse.helpers({
+  schools: function() {
+    var options = { sort: { school: 1 }, fields: { school: true } };
+    return _.uniq(Courses.find({}, options)
+            .map(function(course) { return course.school; }), true);
+  }
+});
---
+++ client/templates/courses/newCourse/newCourse.js
@@ -0,0 +1,28 @@
+Template.newCourse.rendered = function(){
+  $('select.dropdown').dropdown();
+};
+
+Template.newCourse.events({
+  'submit .new-course': function(event) {
+    event.preventDefault();
+    var initials = event.target.initials.value;
+    var name = event.target.name.value;
+    var school = event.target.school.value;
+    var information = event.target.information.value;
+
+    Meteor.call('addCourse', initials, name, school, information);
+
+    event.target.initials.value = "";
+    event.target.name.value = "";
+    event.target.school.value = "";
+    event.target.information.value = "";
+  }
+});
+
+Template.newCourse.helpers({
+  schools: function() {
+    var options = { sort: { school: 1 }, fields: { school: true } };
+    return _.uniq(Courses.find({}, options)
+            .map(function(course) { return course.school; }), true);
+  }
+});
