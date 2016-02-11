2c5e2af00a87b32bf216e3793592a6055ba7f02c
Sebastián Salata R-T <SA.SalataRT@GMail.com>
2016-02-11 14:25:21 -0300

    move some subscriptions around

--- client/lib/router.js
+++ client/lib/router.js
@@ -1,6 +1,32 @@
-var subs = new SubsManager({
+subs = new SubsManager({
   cacheLimit: 20, // only 20 most recent subscriptions will be cached
   expireIn: 10 // any subscription will expire after 10 minutes of inactivity
+});
+
+Router.configure({
+  layoutTemplate: 'layoutTemplate',
+  loadingTemplate: 'loadingTemplate',
+  waitOn: function() {
+    Meteor.subscribe('courses');
+    Meteor.subscribe('users');
+    Tracker.autorun(function() {
+      Meteor.subscribe('messages', Meteor.userId(), {
+        onReady: function() {
+          var messages = Messages.find({ receiver: Meteor.userId() });
+          messages.observeChanges({
+            added: function(id, message) {
+              if (!message.read) {
+                var text = Meteor.users.findOne({ _id: message.notificator }).profile.name;
+                text += ' ha escrito en ';
+                text += Courses.findOne({ _id: message.courseID }).name;
+                Notifications.info('Nueva respuesta', text);
+              }
+            }
+          });
+        }
+      });
+    });
+  }
 });
 
 var OnBeforeActions = {
@@ -22,11 +48,6 @@ var OnBeforeActions = {
   }
 };
 
-Router.configure({
-  layoutTemplate: 'layoutTemplate',
-  loadingTemplate: 'loadingTemplate'
-});
-
 Router.route('/', {
   template: 'landingPage',
   waitOn: function() {
@@ -47,9 +68,9 @@ Router.route('/courses/:_id', {
   },
   waitOn: function() {
     subs.subscribe('course', this.params._id);
-    subs.subscribe('testimonies', this.params._id);
     subs.subscribe('questions', this.params._id);
     subs.subscribe('answers', this.params._id);
+    subs.subscribe('testimonies', this.params._id);
   },
   onAfterAction: function() {
     Session.set('reading', 'questions');
